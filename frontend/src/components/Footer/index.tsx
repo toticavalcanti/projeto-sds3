82165c81dd36b83d18a896fc2778afa60ad447d8
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-09-07 00:29:14 -0300

    basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <footer className="footer mt-auto py-3 bg-dark">
+      <div className="container">
+        <p className="text-light">App desenvolvido por <a href="https://github.com/toticavalcanti" target="_blank" rel="noreferrer">Toti Cavalcanti</a></p>
+        <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+          Evento promovido pela escola DevSuperior: <a href="https://www.linkedin.com/in/antoniocavalcantedepaulafilho/" target="_blank" rel="noreferrer">Linkedin</a></small></p>
+      </div>
+  </footer>
+  );
+}
+
+export default Footer;
