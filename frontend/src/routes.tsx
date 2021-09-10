5cf3f070d61759b1654c4c01a8b3a78ec93f93c1
Antonio Cavalcante de Paula Filho (Toti Cavalcanti) <toticavalcanti@hotmail.com>
2021-09-10 13:46:59 -0300

    routes

---
+++ frontend/src/routes.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Home from './pages/Home';
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact>
+          <Home />  
+        </Route>      
+        <Route path="/dashboard">
+          <Dashboard />  
+        </Route>      
+      </Switch>
+    </BrowserRouter>
+  );
+}
+
+export default Routes;
